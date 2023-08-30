113215f48b9fdde77019ca580f71c220b744b16c
omardaly <136573570+omardaly@users.noreply.github.com>
2023-08-30 16:03:46 +0100

    day_03

---
+++ d_03/first-react/src/components/SomeClassComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class SomeClassComponent extends Component{
+    render(){
+        return(
+            <fieldset>
+                <legend>SomeClassComponet</legend>
+            </fieldset>
+        )
+    }
+}
+export default SomeClassComponent
---
+++ d_03/first-react/src/components/firstClassComponet.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class FirstClassComponent extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello From Component</h1>
+            
+            </div>
+        )
+
+    }
+
+}
+export default FirstClassComponent
