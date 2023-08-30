206b55afd6cc1b6ad10cc30b9743abf79da01b1c
omardaly <136573570+omardaly@users.noreply.github.com>
2023-08-30 16:03:10 +0100

    d_03

---
+++ d_03/core/core/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1>Hello Dojo!</h1>
+      <h3>Things i need to do it : </h3>
+      <ul>
+          <li>Learn react</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the Dogs</li>
+      </ul>
+    </div>
+  );
+  
+}
+
+export default App;
